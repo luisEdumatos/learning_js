07cf5418fdfaa876f2ea58cb08c07a33c78f39ad
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-09 09:56:40 -0300

    Testes iniciais

---
+++ Aula1/index.js
@@ -0,0 +1,19 @@
+//Interpolacao de variaveis
+const lugar = 'Mundo';
+const saudacao = 'Olá';
+
+console.log(`${saudacao}, ${lugar}`);
+
+//Single Line Coments
+var x = 4; //declarando x=4
+var y = x + 2; //resultado da soma de x+2
+console.log(y); //soma total de x+2
+
+//Multi-line Comments
+/* 
+const cachorro = {
+    nome: 'Diamond baby', 
+    raca: 'Chihuaha', 
+    cor: 'marrom/preto'
+}; 
+console.log(cachorro); */
