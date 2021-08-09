2600f178095e3ad9ccdc5cd5c03a268abe7f46cc
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-09 11:10:54 -0300

    Exemplos Strings

---
+++ Aula3/strings.js
@@ -0,0 +1,20 @@
+//Concatenacao de Strings
+/* let nome = 'Luis Eduardo ';
+let sobrenome = 'de Matos';
+
+console.log(nome + sobrenome);  */
+
+//Textos e numeros
+/* let num1 = 1; 
+let num2 = "1"; 
+console.log(num1 + num2); //JavaScript entende que é um texto pois uma das variaveis é textual
+console.log(num1 + 1); //JavaScript entende como um numero pois os dois tipos são numericos*/
+
+//Para evitar problemas com textos + numeros, utiliza-se o metodo concat para garantir que soma de numeros será textual
+/* let str1 = "0 "; 
+let str2 = "plus"; 
+let str3 = " 1 = "; 
+let num1 = 1; 
+
+let res = str1.concat(str2, str3, num1); 
+console.log(res);  */
